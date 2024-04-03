050467080177dad7228576419888b1a47d1d1fdd
ash <70522305+ashsjszds@users.noreply.github.com>
2024-04-03 16:45:59 +0800

    add paper visual

--- Model/Crop_Segmentation/Utils/Evaluate.py
+++ Model/Crop_Segmentation/Utils/Evaluate.py
@@ -2,8 +2,8 @@ import numpy as np
 from PIL import Image
 import os
 
-gt_path = '/Users/shijunshen/Documents/Code/dataset/Smart-Farm/predict_Unet3/gt'
-pred_path = '/Users/shijunshen/Documents/Code/dataset/Smart-Farm/predict_Unet3/pred'
+gt_path = '/Users/shijunshen/Documents/Code/dataset/Smart-Farm/predict_EfficientUnet3+_7/gt'
+pred_path = '/Users/shijunshen/Documents/Code/dataset/Smart-Farm/predict_EfficientUnet3+_7/pred'
 num_classes = 3
 
 def compute():
